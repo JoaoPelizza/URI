7c98fb45aea31e217fc2d4b0487eaa054e9a88f3
JoaoPelizza <joaogpelizza@gmail.com>
2018-09-14 19:25:59 -0300

    2175 passou

--- 2715.cpp
+++ 2715.cpp
@@ -1,28 +1,42 @@
-#include<iostream>
-#include <list>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
 
 using namespace std;
 
 int main(void){
-  //int rang=0,gugu=0;
 
-  int trabalhos;
-  list<int> pesos;
+  long long trabalhos;
+  vector<long long> pesos;
 
-  cin >> trabalhos;
-  for(int aux=0;aux<trabalhos;aux++){
-    int peso;
-    cin >> peso;
-    pesos.push_back(peso);
-  }
+  while( cin >> trabalhos ){
+    pesos.clear();
 
-  for(list<int>::iterator i = pesos.begin();i!=pesos.end();i++){
-    for(list<int>::iterator j = i++ ; j!=pesos.end() ; j++ ){
+    for(long long aux=0, peso=0 ; aux<trabalhos ; aux++){
+      cin >> peso;
+      pesos.push_back(peso);
+    }
+
+    long long sum_i=0 , sum_j=0;
+    long long dif=999999999;
+
+    for(vector<long long>::iterator aux = pesos.begin() ; aux!=pesos.end() ; aux++ ){
+      sum_j+=(*aux);
+    }
+
+    for(vector<long long>::iterator aux = pesos.begin() ; aux!=pesos.end() ; aux++ ){
+      sum_i+=(*aux);
+      sum_j-=(*aux);
+
+      if(abs(sum_i-sum_j)==0){
+        dif=0;
+        break;
+      }
+      else if(abs(sum_i-sum_j)<dif) dif=abs(sum_i-sum_j);
 
     }
 
-    cout << *(i) << endl;
+    cout << dif << endl;
+    }
+    return 0;
   }
-
-  return 0;
-}
